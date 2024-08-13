f6c28abdbfdf0151a7d374662dd5d0eecb98f833
Rommariomc <rommario.mocas@gmail.com>
2024-08-12 23:40:25 -0500

    Create Rommariomc.py
    Reto 1, estoy aprendiendo en Comunidad

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Rommariomc.py
@@ -0,0 +1,21 @@
+# Mi primer comentario
+"""" 
+URL oficial de pyton https://www.python.org/
+""""
+
+#Comentario de 1 línea
+"""
+Comentario de varias líneas opción 1
+"""
+'''
+Comentario de varias líneas opción 2
+'''
+
+mi_primer_variable = "Hola"
+
+#No hay constantes en python, pero se pueden crear variables a las que no queremos que se cambie el valor y tendrían un asignación de constante
+
+var1 = "Esto es un String"
+var2 = 23 + " Esto es un entero"
+Var3 = 3.111 + " Esto es un float"
+Var = True " Esto es un booleano"
